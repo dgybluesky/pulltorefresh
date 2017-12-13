d0f78bbd2469fcd862e2a88909c9fe715e70cbb2
dgy <dinggy@nanruan.net>
2017-12-13 15:50:48 +0800

    create projects

---
+++ pulltorefreshlibrary/src/main/java/me/dgy/pulltorefreshlibrary/MaterialRefreshListener.java
@@ -0,0 +1,7 @@
+package me.dgy.pulltorefreshlibrary;
+
+public abstract class MaterialRefreshListener {
+    public void onfinish(){};
+    public abstract void onRefresh(MaterialRefreshLayout materialRefreshLayout);
+    public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout){};
+}
